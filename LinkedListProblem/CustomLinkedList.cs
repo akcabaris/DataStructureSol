af6c2d5cb27b1d9ed14de670666b4c66e63cd1f4
Barış Akça <barisakcaa@icloud.com>
2024-11-21 17:01:20 +0300

    CustomLinkedListSolution

---
+++ LinkedListProblem/CustomLinkedList.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+class CustomLinkedList<T> : IEnumerable
+{
+    private LinkedList<T> list;
+
+    public CustomLinkedList(T[] input)
+    {
+
+        list = new LinkedList<T>();
+        var hashSet = new HashSet<T>(input);
+        var stack = new Stack<T>(hashSet);
+        // if we just write stack.Count in the for loop. it's gonna change during the for loop working. that's why it should keep in the variable
+        int n = stack.Count;
+        for(int i=0; i<n; i++)
+        {
+            list.AddLast(stack.Pop());
+        }
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        return list.GetEnumerator();
+    }
+}
---
+++ LinkedListProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿var customLinkedList = new CustomLinkedList<char>("wellcome".ToCharArray());
+var customLinkedList2 = new CustomLinkedList<int>(new int[] {1,2,3,3,2,4,5});
+
+// m o c l e w
+foreach (var item in customLinkedList)
+{
+    Console.Write($"{item,-2}");
+}
+Console.WriteLine();
+
+// 5 4 3 2 1
+foreach (var item in customLinkedList2)
+{
+    Console.Write($"{item,-2}");
+}
+
+Console.ReadKey();
